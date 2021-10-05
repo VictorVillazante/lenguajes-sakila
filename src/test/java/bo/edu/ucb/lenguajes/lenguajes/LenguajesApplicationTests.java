46f4527adb89541bc76f1c382b516641da87cc3f
Victor <villazante.bernabe.victor@gmail.com>
2021-10-04 23:41:41 -0400

    primera version

---
+++ src/test/java/bo/edu/ucb/lenguajes/lenguajes/LenguajesApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.lenguajes.lenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenguajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
